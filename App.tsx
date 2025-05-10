40544093974eedf7e52df11e4e856f4127303c74
Saurav Kanchan <sauravnk30@gmail.com>
2025-05-10 08:51:29 +0100

    not-working

--- App.tsx
+++ App.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import AppNavigator from './src/AppNavigator';
+import 'react-native-url-polyfill/auto';
+import { Buffer } from 'buffer';
+import process from 'process';
+
+global.Buffer = Buffer;
+global.process = process;
+
+import 'node-libs-react-native/globals';
+import 'react-native-url-polyfill/auto';
+import 'react-native-get-random-values';
 
 const App: React.FC = () => <AppNavigator />;
 
--- metro.config.js
+++ metro.config.js
@@ -7,9 +7,57 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
   resolver: {
+    extraNodeModules: {
+      ...require('node-libs-react-native'),
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      process: require.resolve('process/browser'),
+      buffer: require.resolve('buffer'),
+      assert: require.resolve('assert'),
+    },
     unstable_enablePackageExports: false,
   },
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
+// const { getDefaultConfig } = require('metro-config');
+
+// module.exports = (async () => {
+//   const {
+//     resolver: { sourceExts, assetExts },
+//   } = await getDefaultConfig();
+
+//   return {
+//     transformer: {
+//       getTransformOptions: async () => ({
+//         transform: {
+//           experimentalImportSupport: false,
+//           inlineRequires: true,
+//         },
+//       }),
+//     },
+//     resolver: {
+//       extraNodeModules: {
+//         ...require('node-libs-react-native'),
+//         stream: require.resolve('stream-browserify'),
+//         crypto: require.resolve('crypto-browserify'),
+//         process: require.resolve('process/browser'),
+//         buffer: require.resolve('buffer'),
+//         assert: require.resolve('assert'),
+//       },
+//       assetExts,
+//       sourceExts,
+//       unstable_enablePackageExports: false,
+//     },
+//   };
+// })();
